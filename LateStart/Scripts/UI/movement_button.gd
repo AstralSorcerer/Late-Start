d3a019661530bfcb32b7e407260a7237172f1c46
Chloe Pronovost <chloe.pronovost@pm.me>
2023-09-17 16:33:46 -0600

    Fix movement
    Godot doesn't like scenes with cyclic references, so we have to
    reference target scenes by path name.

--- LateStart/Scripts/UI/movement_button.gd
+++ LateStart/Scripts/UI/movement_button.gd
@@ -1,10 +1,10 @@
 class_name MovementButton extends TextureButton
 
-@export var target: PackedScene
+@export var target: String
 
 
 func _pressed():
 	var room := get_tree().current_scene.get_node("Room")
 	if room:
 		room.remove_child(room.get_child(0))
-		room.add_child(target.instantiate())
+		room.add_child(load(target).instantiate())
