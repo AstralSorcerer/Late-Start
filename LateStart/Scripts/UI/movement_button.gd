955cf7545376ca576fd5757b9401c91146325611
Chloe Pronovost <chloe.pronovost@pm.me>
2023-09-17 03:09:29 -0600

    Add "move to dining room" button

--- LateStart/Scripts/UI/movement_button.gd
+++ LateStart/Scripts/UI/movement_button.gd
@@ -1,3 +1,10 @@
 class_name MovementButton extends TextureButton
 
 @export var target: PackedScene
+
+
+func _pressed():
+	var room := get_tree().current_scene.get_node("Room")
+	if room:
+		room.remove_child(room.get_child(0))
+		room.add_child(target.instantiate())
